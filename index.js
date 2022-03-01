d1a307240405e5513ccb1f2582468a6ec88884de
Jimmy Lin <b00502013@gmail.com>
2022-03-01 11:00:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 #!/usr/bin/env node
 import fs from "fs";
-import path from "path"
+import path from "path";
 
 const CWD = process.cwd();
 const CONFIGS = [
   {
-    filePath: path.resolve(CWD, ".vscode", "settings.json"), 
+    filePath: path.resolve(CWD, ".vscode", "settings.json"),
     defaultData: {
       "prettier.tabWidth": 2,
+      "editor.linkedEditing": true,
       "python.formatting.provider": "black",
-      "python.formatting.blackArgs": [
-        "--line-length=79"
-      ],
-      "python.linting.flake8Enabled": true,
+      "python.formatting.blackArgs": ["--line-length=79"],
       "python.linting.enabled": true,
-      "editor.linkedEditing": true
-    }
-  }
-]
+      "python.linting.flake8Enabled": true,
+      "python.linting.flake8Args": ["--ignore=E203"],
+    },
+  },
+];
 
 const parseConfig = (filePath) => {
   const content = fs.existsSync(filePath) ? fs.readFileSync(filePath, "utf8") : "";
-  const data = content ? JSON.parse(content) : {}
-  return data
-}
+  const data = content ? JSON.parse(content) : {};
+  return data;
+};
 
 const stringifyConfig = (filePath, data) => {
-  const content = JSON.stringify(data, null, 2)
+  const content = JSON.stringify(data, null, 2);
   if (!fs.existsSync(filePath)) {
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
   }
   return fs.writeFileSync(filePath, content);
-}
+};
 
 for (const config of CONFIGS) {
-  const oldData = parseConfig(config.filePath)
-  const newData = {...config.defaultData, ...oldData}
-  stringifyConfig(config.filePath, newData)
+  const oldData = parseConfig(config.filePath);
+  const newData = { ...config.defaultData, ...oldData };
+  stringifyConfig(config.filePath, newData);
 }
